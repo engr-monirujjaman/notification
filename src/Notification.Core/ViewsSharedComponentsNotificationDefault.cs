99235b702cea9773b80e751cac0e085435748d09
Monirujjaman <md.monirujjaman@alaricsecurities.com>
2022-08-16 18:18:40 +0600

    updated

--- src/Notification.Core/ViewsSharedComponentsNotificationDefault.cs
+++ src/Notification.Core/ViewsSharedComponentsNotificationDefault.cs
@@ -1,14 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc.Razor;
-using Microsoft.AspNetCore.Razor.Hosting;
-
-namespace Notification.Core;
-
-[RazorSourceChecksum("SHA1", "CA254A1CFC664B70A030C4F84961CD03", "/Views/Shared/Components/Notification/Default.cshtml" )]
-public class  ViewsSharedComponentsNotificationDefault : RazorPage<NotificationViewModel>
-{
-    public override async Task ExecuteAsync()
-    {
-        Write("<div>Hello World!</div>");
-        await Task.CompletedTask;
-    }
-}
+﻿// using Microsoft.AspNetCore.Components;
+// using Microsoft.AspNetCore.Mvc.Razor;
+// using Microsoft.AspNetCore.Razor.Hosting;
+//
+// namespace Notification.Core;
+//
+// [RazorSourceChecksum("SHA1", "CA254A1CFC664B70A030C4F84961CD03", "/Views/Shared/Components/Notification/Default.cshtml" )]
+// public class  ViewsSharedComponentsNotificationDefault : RazorPage<NotificationViewModel>
+// {
+//     
+//     private NotificationConfig _config;
+//     
+//     public override async Task ExecuteAsync()
+//     {
+//         var notificationConfig = @"const notificationConfig = {
+//                                 
+//             
+//
+//         }";
+//         
+//         Write("<div>Hello World!</div>");
+//         await Task.CompletedTask;
+//     }
+// }
